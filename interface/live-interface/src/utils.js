edafda2f6e7fd779c018ff0c3b42c1edc30e3eb0
Vetoa <vetoa@protonmail.com>
2023-09-15 11:35:02 -0500

    removed old files

--- interface/live-interface/src/utils.js
+++ interface/live-interface/src/utils.js
@@ -1,24 +1,3 @@
-
-
-import { timeParse } from "d3-time-format";
-
-
-function parseData(data) {
-  // const parseDate = timeParse("%Y-%m-%d %H:%M:%S");
-
-  return data.map(d => {
-    return {
-      date: parseInt(d.timestamp), 
-      open: parseInt(d.opening), 
-      high: parseInt(d.high), 
-      low: parseInt(d.low), 
-			close: parseInt(d.closing), 
-      volume: parseFloat(d.volume) 
-    };
-  })
-}
-
-
 export async function getData() {
 	return fetch("http://127.0.0.1:8000/api/klinetrades/get")
 		.then(response => {
@@ -63,18 +42,3 @@ export async function getOrders() {
     const bids = unfiltered_bids.filter(item => item[0] <= lowest_ask)
     return { asks, bids, qty }
 }
-
-
-export function getData2() {
-  // const json_fetch = fetch("http://127.0.0.1:8000/trades")
-  return fetch("https://demo-live-data.highcharts.com/aapl-ohlc.json")
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      return data;
-    })
-}
-
-
-
